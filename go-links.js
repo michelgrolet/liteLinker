03251ded2b36d24eac9a33c3338ebc4f89fe44ea
Michel Grolet <39418855+michelgrolet@users.noreply.github.com>
2024-10-09 11:37:13 +0200

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -1,21 +1,69 @@
+// EDIT THESE !
+const goLinksURL = "go.youdomain.com";
+const goLinkNotFoundURL = "yourdomain.com";
+
+// ADD YOUR GO LINKS HERE
 const goLinks = {
+	// settings
+	'add': 'https://github.com/MichelGrolet/go/edit/main/go-links.js',
+	'new': 'add',
+	'edit': 'add',
+	// socials
+	'insta': 'https://www.instagram.com/mi.grlt/',
+	'linkedin': 'https://www.linkedin.com/in/michelgrolet/',
+	'ln': 'linkedin',
+	'twitter': 'https://x.com/michelgrolet',
+	'x': 'twitter',
+	'github': 'https://github.com/michelgrolet',
+	'gh': 'github',
+	// misc
 	'wikirandom': 'https://en.wikipedia.org/wiki/Special:Random',
 	'photographer': 'https://photographe.michelgrolet.fr',
-	// add more shortcuts as needed
   };
-  
-  function redirect() {
-	const basePath = 'liteLinker'; // repository name
-	let path = window.location.pathname.substr(1); // get the part after the initial slash
-	
-	if (path.startsWith(basePath)) {
-	  path = path.substr(basePath.length + 1); // +1 to account for the slash
-	}
-  
-	const redirectURL = goLinks[path];
-	if (redirectURL) {
-	  window.location.href = redirectURL;
-	} else {
-	  document.body.innerHTML = 'Go link not found.';
-	}
-  }
+
+function redirect() {
+    const basePath = 'go'; // repository name
+    let path = window.location.pathname.substr(1); // get the part after the initial slash
+
+    if (path === 'links') return; // Prevent redirect on /links
+
+    if (path.startsWith(basePath)) {
+        path = path.substr(basePath.length + 1); // +1 to account for the slash
+    }
+
+    const redirectURL = goLinks[path];
+    if (redirectURL) {
+        window.location.href = redirectURL;
+    } else {
+        window.location.href = goLinkNotFoundURL;
+    }
+}
+
+function generateLinksPage() {
+    let html = '<!DOCTYPE html><html><head><title>Go Links</title></head><body>';
+    html += '<h1>Go Links</h1><ul>';
+
+    for (const [key, url] of Object.entries(goLinks)) {
+        html += `<li><a href="${url}">${key}</a></li>`;
+    }
+
+    html += '</ul></body></html>';
+    return html;
+}
+
+const http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === '/links') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(generateLinksPage());
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+}).listen(80, () => {
+    console.log(`Server is running at ${goLinksURL}`);
+});
+
+// Call redirect function on page load
+redirect();
